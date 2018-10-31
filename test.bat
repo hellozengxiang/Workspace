0a3d083b738d1c54cf3cff484293f1a6e952178c
junzeng <jun.zeng@fdm.com.cn>
2018-10-31 21:36:16 +0800

    add 2 files

---
+++ test.bat
@@ -0,0 +1,20 @@
+
+REM Set excelIndex = "D:\test.xlsm"
+REM Set macroName = "test_macro"
+REM Set excelFile = "test.xlsm"
+
+
+REM Set excelApp = CreateObject("Excel.Application")
+
+
+REM Set objWbk = excelApp.Workbooks.Open(excelIndex, True, False)
+
+REM excelApp.run "test.xlsm!test_macro"
+
+REM pause
+
+
+REM "C:\Program Files (x86)\Microsoft Office\root\Office16\EXCEL.EXE"  "D:\test.xlsm" /batOpen
+
+echo helloworld
+
